e12d1d29f132381a3c9ca31fc4423b953fb3e0d5
半土 <kurtcobain1988824@hotmail.com>
2018-03-26 01:34:33 +0800

    2018/3/26

--- zhihuCrawl/zhihuCrawl/items.py
+++ zhihuCrawl/zhihuCrawl/items.py
@@ -8,11 +8,41 @@
 import scrapy
 
 
-class ZhihucrawlItem(scrapy.Item):
+class TopicItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    topic = scrapy.Field()
-    articleList = scrapy.Field()
-    name = scrapy.Field()
-    name = scrapy.Field()
+    topicID = scrapy.Field()
+    topicName = scrapy.Field()
+    topicClass = scrapy.Field()
+
+    pass
+
+class ArtLinkItem(scrapy.Item):
+
+    artLinkTitle = scrapy.Field()
+    artLinkURL = scrapy.Field()
+    artLinkID = scrapy.Field()
+
+    pass
+
+class ArticleItem(scrapy.Item):
+    articleID = scrapy.Field()
+    articleTitle = scrapy.Field()
+    articleURL = scrapy.Field()
+    artTopID = scrapy.Field()
+
+    pass
+
+class Question(scrapy.Item):
+    quesID = scrapy.Field()
+    quesTitle = scrapy.Field()
+    quesLink = scrapy.Field()
+    quesDetail = scrapy.Field()
+    pass
+
+
+class Answer(scrapy.Item):
+    ansID = scrapy.Field()
+    ansAuthor = scrapy.Field()
+    ansContent = scrapy.Field()
     pass
