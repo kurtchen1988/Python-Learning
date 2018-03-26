394fddcaa755b7d70b37be45c54404a2847fa8c6
半土 <kurtcobain1988824@hotmail.com>
2018-03-26 20:40:21 +0800

    2018/3/26

--- zhihuCrawl/zhihuCrawl/pipelines.py
+++ zhihuCrawl/zhihuCrawl/pipelines.py
@@ -4,8 +4,23 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import zhihuCrawl.items
 
 class ZhihucrawlPipeline(object):
     def process_item(self, item, spider):
+
+        if isinstance(item, zhihuCrawl.items.TopicItem):
+            print(zhihuCrawl.items.TopicItem.get("topicID"))
+            print(zhihuCrawl.items.TopicItem.get("topicName"))
+            print(zhihuCrawl.items.TopicItem.get("topicClass"))
+            print(1)
+        elif isinstance(item, zhihuCrawl.zhihuCrawl.items.ArtLinkItem):
+            print(2)
+        elif isinstance(item, zhihuCrawl.zhihuCrawl.items.ArticleItem):
+            print(3)
+        elif isinstance(item, zhihuCrawl.zhihuCrawl.items.Question):
+            print(4)
+        elif isinstance(item, zhihuCrawl.zhihuCrawl.items.Answer):
+            print(5)
+
         return item
