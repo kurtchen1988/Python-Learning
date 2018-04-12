d70edefe6e71ef816589d31013894dc56ba82226
半土 <kurtcobain1988824@hotmail.com>
2018-04-12 17:21:44 +0800

    2018-04-10

--- myweb/myapp/views.py
+++ myweb/myapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.models import Stu
 # Create your views here.
 def index(request):
     return HttpResponse("Hello Django")
+
+def stu(request):
+    mod = Stu.objects
+    list = mod.all()
+    print(list)
+
+    return HttpResponse("OK")
