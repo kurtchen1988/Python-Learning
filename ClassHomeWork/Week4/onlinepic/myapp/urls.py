d8915e6c201ea782f3a1d5af1f363be3ce765dbc
半土 <kurtcobain1988824@hotmail.com>
2018-04-16 18:45:34 +0800

    2018-04-15

--- ClassHomeWork/Week4/onlinepic/myapp/urls.py
+++ ClassHomeWork/Week4/onlinepic/myapp/urls.py
@@ -6,10 +6,15 @@ from . import views
 urlpatterns = [
     #进入系统首页
     url(r'^$',views.index, name='index'),
-    #进入相册列表页
+    # 进入相册列表页
     url(r'^piclist/(?P<pIndex>[0-9]+)$',views.piclist, name='piclist'),
     # 文件上传
     url(r'^showpic$', views.showpic, name="showpic"),
     url(r'^upload$', views.addpic, name="upload"),
+    # 删除图片
+    url(r'delpic/(?P<pIndex>[0-9]+)$',views.delpic,name='delpic'),
+    # 修改图片
+    url(r'editpic/(?P<pIndex>[0-9]+)$',views.editpic,name='editpic'),
+    url(r'modify$',views.subeditpic,name='subeditpic')
 
 ]
--- ClassHomeWork/Week4/onlinepic/myapp/views.py
+++ ClassHomeWork/Week4/onlinepic/myapp/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from myapp.models import Photos
+from datetime import datetime
 from django.core.paginator import Paginator
 from django.views.decorators.csrf import csrf_exempt
 import time,os
 from PIL import Image
 # Create your views here.
 def index(request):
+    '''
+
+    :param request:
+    :return: 返回到图片展示页面的第一页
+    '''
     return render(request,"index.html")
 
 def showpic(request):
+    '''
+
+    :param request: 网页响应参数
+    :return: 返回到相册添加页面
+    '''
     return render(request, "myapp/add.html")
 
 def piclist(request, pIndex):
+    '''
+
+    :param request: 网页响应参数
+    :param pIndex:
+    :return:
+    '''
     use=Photos.objects.all()
     list = Photos.objects.all()
     p = Paginator(list, 5)
@@ -24,8 +41,47 @@ def piclist(request, pIndex):
     context = {"ulist": list2, "plist": plist, "pIndex": int(pIndex)}
     return render(request, 'myapp/index.html', context)
 
+def delpic(request, pIndex):
+
+
+    pass
+
+def editpic(request, pIndex):
+    '''
+    加载相册修改信息
+    :param request:
+    :param pIndex:
+    :return:
+    '''
+    if pIndex == ":":
+        return HttpResponse("没有上传文件信息")
+    print(pIndex)
+    plist = Photos.objects.get(id=int(pIndex))
+    context = {"plist":plist}
+    return render(request,"myapp/modify.html",context)
+
+@csrf_exempt
+def subeditpic(request):
+    '''
+    执行修改相册信息
+    :param request:
+    :return:
+    '''
+    print(request.POST['id'])
+    ob = Photos.objects.get(id=request.POST['id'])
+    ob.name = request.POST['title']
+    ob.pic = request.POST['pic']
+    ob.save()
+    return HttpResponseRedirect("piclist/1")
+
 @csrf_exempt
 def addpic(request):
+    '''
+    添加图片功能
+    :param request: 网页响应参数
+    :return: 返回到图片展示页面的第一页
+    '''
+    #首先上传文件到相应目录（要确保static/pictures这个目录在项目根目录下，并且有读写权限）
     myfile = request.FILES.get("mypic", None)
     print(myfile)
     if not myfile:
@@ -36,11 +92,12 @@ def addpic(request):
         destination.write(chunk)
     destination.close()
 
-    # 执行图片缩放
-    im = Image.open("./static/pics/" + filename)
-    # 缩放到75*75(缩放后的宽高比例不变):
-    im.thumbnail((75, 75))
-    # 把缩放后的图像用jpeg格式保存:
-    im.save("./static/pics/s_" + filename, None)
+    #接着把所有信息写入数据库，图片仅存名字
+    ob = Photos()
+    ob.title = request.POST['title']
+    ob.pic = filename
+    ob.timeupload = datetime.now()
 
-    return HttpResponse("ok")
+    ob.save()
+
+    return HttpResponseRedirect("piclist/1")
