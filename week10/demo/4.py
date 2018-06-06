7677a33e2269d22a16e0940290920e5fc3522a9d
半土 <kurtcobain1988824@hotmail.com>
2018-06-06 17:09:17 +0800

    2018-06-06

---
+++ week10/demo/4.py
@@ -0,0 +1,21 @@
+import random, requests
+
+proxy_list = [
+    '182.39.6.245:38634',
+    '115.210.181.31:34301',
+    '123.161.152.38:23201',
+    '222.85.5.187:26675',
+    '123.161.152.31:23127',
+]
+
+proxy = random.choice(proxy_list)
+
+proxies = {
+    'http':'http://'+proxy,
+    'https':'https://'+proxy,
+}
+try:
+    response = requests.get('http://httpbin.org/get', proxies=proxies)
+    print(response.text)
+except requests.exceptions.ConnectionError as e:
+    print('Error', e.args)
