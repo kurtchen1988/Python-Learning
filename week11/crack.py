705415c2632fce51b898f27e33167387b6aa3809
Kurt Chen <kurtcobain1988824@hotmail.com>
2018-06-23 18:32:42 +0800

    2018-06-06

--- week11/crack.py
+++ week11/crack.py
@@ -9,3 +9,16 @@ from selenium.webdriver.support import expected_conditions as EC
 
 EMAIL = '122794105@qq.com'
 PASSWORD = 'python123'
+BORDER = 6
+
+class CrackGeetest():
+    def __init__(self):
+        self.url = 'https://account.geetest.com/login'
+        self.browser = webdriver.Chrome()
+        #设置显示等待
+        self.wait = WebDriverWait(self.browser, 20)
+        self.email = EMAIL
+        self.password = PASSWORD
+
+    def __del__(self):
+        pass
