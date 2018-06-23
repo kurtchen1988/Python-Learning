5075293d366b9ee7f75ae1df56bef1fc3efadaf3
Kurt Chen <kurtcobain1988824@hotmail.com>
2018-06-23 18:28:17 +0800

    2018-06-06

---
+++ week11/crack.py
@@ -0,0 +1,11 @@
+import time
+from io import BytesIO
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+EMAIL = '122794105@qq.com'
+PASSWORD = 'python123'
