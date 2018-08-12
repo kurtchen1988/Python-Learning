b9192b288708a647a18121b8491ab5e9991c17cb
bantu <76251098@qq.com>
2018-08-12 19:32:17 +0800

    2018-8-12

--- week15/gen.py
+++ week15/gen.py
@@ -1,9 +1,41 @@
 import random
 
-file = open('data/x_height.txt','w+')
+cities = ['杭州市',
+'宁波市',
+'绍兴市',
+'温州市',
+'台州市',
+'湖州市',
+'嘉兴市',
+'金华市',
+'舟山市',
+'衢州市',
+'丽水市',
+'余姚市',
+'乐清市',
+'临海市',
+'温岭市',
+'永康市',
+'瑞安市',
+'慈溪市',
+'义乌市',
+'上虞市',
+'诸暨市',
+'海宁市',
+'桐乡市',
+'兰溪市',
+'龙泉市',
+'建德市',
+'富德市',
+'富阳市',
+'平湖市',
+'东阳市',
+'嵊州市',
+'奉化市',
+'临安市',
+'江山市']
+file = open('data/cities.txt','w+')
+file.write('排名 城市 平均工资'+'\n')
 
-for i in range(18):
-    file.write(str(i) + ' ' + str(random.randint(1,170))+'\n')
-
-for i in range(18):
-    file.write(str(i) + ' ' + str(random.randint(1,170))+'\n')
+for i in range(len(cities)):
+    file.write(str(i) + ' '+random.choice(cities)+' '+ str(random.randint(1,10000))+'\n')
